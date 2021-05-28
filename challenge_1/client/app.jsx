419e8c036fe39b18dfc8bd0364f76a46c98a8999
Grant Taggart <granttaggartsf@gmail.com>
2021-05-27 18:26:48 -0700

    Added methods for handling search

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -3,74 +3,57 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Events from './events.jsx';
 import ReactPaginate from 'react-paginate';
-import $ from 'jquery';
 
 export default class App extends React.Component {
-
-  _isMounted = false;
 
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      offset: 0,
+      events: [],
     };
 
-    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.loadEvents = this.loadEvents.bind(this);
     this.handlePageClick = this.handlePageClick.bind(this);
+    this.search = this.search.bind(this);
+    this.searchTextOnChange = this.searchTextOnChange.bind(this);
   }
 
-  loadCommentsFromServer() {
-    $.ajax({
-      url: this.props.url,
-      data: { limit: this.props.perPage, offset: this.state.offset },
-      dataType: 'json',
-      type: 'GET',
-
-      success: (data) => {
-        if (this._isMounted) {
-
-          this.setState({
-            data: data,
-            pageCount: Math.ceil(data.length / 10),
-          });
-        }
-
-      },
-
-      error: (xhr, status, err) => {
-        console.error(this.props.url, status, err.toString()); // eslint-disable-line
-      },
-    });
+  loadEvents(page, query) {
+    console.log(this.props.url`?_page=${page}&_limit=${this.props.perPage}&q=${query}`)
+    fetch(this.props.url`?_page=${page}&_limit=${this.props.perPage}&q=${query}`)
+      .then((result) => {
+        console.log(result.json());
+      })
+      .catch((err) => console.log('Error: err'));
   }
 
-  componentDidMount() {
-    this._isMounted = true;
-
-    this.loadCommentsFromServer();
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  handlePageClick = (data) => {
-    let selected = events.selected;
-    let offset = Math.ceil(selected * this.props.perPage);
-
-    this.setState({ offset: offset }, () => {
-      this.loadCommentsFromServer();
-    });
+  handlePageClick(e) {
+    console.log(e.selected);
+    this.loadEvents(e.selected + 1, this.state.searchText)
   };
+
+  search(e) {
+    e.preventDefault();
+    this.loadEvents(0, this.state.searchText);
+  }
+
+  searchTextOnChange(e) {
+    this.setState({searchText: e.target.value});
+  }
 
   render () {
     return (
       <div>
         <h1>Historical Events Finder</h1>
-        <Events events={this.state.data} />
+        <form onSubmit={this.search}>
+          <label htmlFor="search">Search Event Keywords</label>
+          <input onChange={this.searchTextOnChange} name="search" value={this.state.searchText}></input>
+          <button type="submit">Search</button>
+        </form>
+        <Events events={this.state.events} />
         <ReactPaginate
-          previousLabel={'previous'}
-          nextLabel={'next'}
+          previousLabel={'Prev'}
+          nextLabel={'Next'}
           breakLabel={'...'}
           breakClassName={'break-me'}
           pageCount={this.state.pageCount}
