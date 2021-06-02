f9d1be0553ff3fc445b76247017b1c0431047562
Grant Taggart <granttaggartsf@gmail.com>
2021-06-02 15:15:28 -0700

    Complete Express server setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require("express");
+const app = express(); // create express app
+const path = require("path");
 
+//add midleware
+app.use(express.static(path.join(__dirname, "..", "client")));
+app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+// start express server on port 3000
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
