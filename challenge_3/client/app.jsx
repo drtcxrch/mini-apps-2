dd79f593e8fac075a812582190b4aa15103b5e4c
Grant Taggart <granttaggartsf@gmail.com>
2021-07-13 14:35:59 -0400

    Fix bug so that repeated values are accepted as new values

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -8,7 +8,8 @@ class App extends React.Component {
     this.state = {
       pinsStanding: 10,
       score: 0,
-      currentFrame: 1
+      currentFrame: 1,
+      bowl: ''
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,27 +18,40 @@ class App extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.form.reset();
-
+    this.setState({bowl: ''});
   }
 
   handleBowlChange(e) {
-    var bowl = e.target.value;
+    var currentRoll = e.target.value;
     var frame = `frame_${this.state.currentFrame}`;
-    var currentFrame = this.state.currentFrame;
+    console.log(currentRoll);
 
-    if (!this.state.[frame]) {
-      this.setState({[frame]: {roll_1: bowl}}, () => console.log(this.state))
-    } else {
-      this.setState({[frame]: {...this.state.[frame], roll_2: bowl}}, () => {
-        var newFrame = this.state.currentFrame + 1;
-        this.setState({currentFrame: newFrame }, () => {
-          frame = `frame_${this.state.currentFrame + 1}`;
-          console.log(this.state)});
-      });
-
-    }
-
+    this.setState({bowl: currentRoll}, () => {
+      if (!this.state.[frame]) {
+        if (currentRoll === 'x') {
+          var newFrame = this.state.currentFrame + 1;
+          this.setState({ [frame]: { isStrike: true }, currentFrame: newFrame }, () => {
+            console.log(this.state);
+          });
+        } else {
+          this.setState({ [frame]: { roll_1: this.state.bowl }, pinsStanding: this.state.pinsStanding - currentRoll }, () => console.log(this.state))
+        }
+      } else if (this.state.pinsStanding - currentRoll < 0) {
+        alert(`Too many pins entered: please enter a number less than ${this.state.pinsStanding + 1}`)
+      } else {
+        if (this.state.pinsStanding - currentRoll === 0) {
+          console.log('true');
+          this.setState({ [frame]: { ...this.state.[frame], roll_2: '/', isSpare: true }, pinsStanding: 10 });
+        } else {
+          this.setState({ [frame]: { ...this.state.[frame], roll_2: this.state.bowl }, pinsStanding: 10 }, () => {
+            var newFrame = this.state.currentFrame + 1;
+            this.setState({ currentFrame: newFrame }, () => {
+              console.log(this.state)
+            });
+          });
+        }
+      }
+    });
   }
 
   render () {
@@ -49,12 +63,12 @@ class App extends React.Component {
           <form ref={form => this.form = form}
           onSubmit={this.handleSubmit}>
             <label>
-              Bowl:
+              Enter number of pins hit or an 'x' for a strike:
             <input
               type="text"
               className="roll-score"
-              placeholder="Enter Pin Count Here"
-              value={`this.state.frames.frame_${this.state.currentFrame}`.roll_1 === null ? `this.state.frames.frame_${this.state.currentFrame}`.roll_1 : `this.state.frames.frame_${this.state.currentFrame}`.roll_2}
+              placeholder="What did you roll?"
+              value={this.state.bowl}
               onChange={this.handleBowlChange}
               />
             </label>
--- challenge_3/client/table.jsx
+++ challenge_3/client/table.jsx
@@ -1,50 +1,93 @@
 import React from 'react';
 
 const Table = (props) => {
+
   var score = props.score;
   console.log(score);
   return (
     <table id='scorecardTable' className='scorecard' cellPadding='1' cellSpacing='0'>
       <tbody>
-      <tr>
-        <th colSpan='6'>Frame 1</th>
-        <th colSpan='6'>Frame 2</th>
-        <th colSpan='6'>Frame 3</th>
-        <th colSpan='6'>Frame 4</th>
-        <th colSpan='6'>Frame 5</th>
-        <th colSpan='6'>Frame 6</th>
-        <th colSpan='6'>Frame 7</th>
-        <th colSpan='6'>Frame 8</th>
-        <th colSpan='6'>Frame 9</th>
-        <th colSpan='6'>Frame 10</th>
-      </tr>
-      <tr>
-        <td colSpan='3'></td><td id="frame1" colSpan='3'>{score.frame_1 ? score.frame_1.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame2" colSpan='3'>{score.frame_2 ? score.frame_2.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame3" colSpan='3'>{score.frame_3 ? score.frame_3.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame4" colSpan='3'>{score.frame_4 ? score.frame_4.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame5" colSpan='3'>{score.frame_5 ? score.frame_5.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame6" colSpan='3'>{score.frame_6 ? score.frame_6.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame7" colSpan='3'>{score.frame_7 ? score.frame_7.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame8" colSpan='3'>{score.frame_8 ? score.frame_8.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame9" colSpan='3'>{score.frame_9 ? score.frame_9.roll_2 : null}</td>
-          <td colSpan='3'></td><td id="frame10" colSpan='3'>{score.frame_10 ? score.frame_10.roll_2 : null}</td>
-      </tr>
-      <tr>
-        <td colSpan='6' id="marker0">{score.frame_1 ? score.frame_1.roll_1 : null}</td>
-        <td colSpan='6' id="marker1">{score.frame_2 ? score.frame_2.roll_1 : null}</td>
-        <td colSpan='6' id="marker2">{score.frame_3 ? score.frame_3.roll_1 : null}</td>
-        <td colSpan='6' id="marker3">{score.frame_4 ? score.frame_4.roll_1 : null}</td>
-        <td colSpan='6' id="marker4">{score.frame_5 ? score.frame_5.roll_1 : null}</td>
-        <td colSpan='6' id="marker5">{score.frame_6 ? score.frame_6.roll_1 : null}</td>
-        <td colSpan='6' id="marker6">{score.frame_7 ? score.frame_7.roll_1 : null}</td>
-        <td colSpan='6' id="marker7">{score.frame_8 ? score.frame_8.roll_1 : null}</td>
-        <td colSpan='6' id="marker8">{score.frame_9 ? score.frame_9.roll_1 : null}</td>
-        <td colSpan='6' id="marker9">{score.frame_10 ? score.frame_10.roll_1 : null}</td>
-      </tr>
+        <tr>
+          <th colSpan='6'>Frame 1</th>
+          <th colSpan='6'>Frame 2</th>
+          <th colSpan='6'>Frame 3</th>
+          <th colSpan='6'>Frame 4</th>
+          <th colSpan='6'>Frame 5</th>
+          <th colSpan='6'>Frame 6</th>
+          <th colSpan='6'>Frame 7</th>
+          <th colSpan='6'>Frame 8</th>
+          <th colSpan='6'>Frame 9</th>
+          <th colSpan='6'>Frame 10</th>
+        </tr>
+        <tr>
+          <td colSpan='3'></td><td id="frame1" colSpan='3'>
+            {score.frame_1 && score.frame_1.isStrike ?  'X' : null}
+            {score.frame_1 ? score.frame_1.roll_1 :null}
+            {score.frame_1 && score.frame_1.roll_2 ? `, ${score.frame_1.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame2" colSpan='3'>
+            {score.frame_2 && score.frame_2.isStrike ? 'X' : null}
+            {score.frame_2 ? score.frame_2.roll_1 :null}
+            {score.frame_2 && score.frame_2.roll_2 ? `, ${score.frame_2.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame3" colSpan='3'>
+            {score.frame_3 && score.frame_3.isStrike ? 'X' : null}
+            {score.frame_3 ? score.frame_3.roll_1 :null}
+            {score.frame_3 && score.frame_3.roll_2 ? `, ${score.frame_3.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame4" colSpan='3'>
+            {score.frame_4 && score.frame_4.isStrike ? 'X' : null}
+            {score.frame_4 ? score.frame_4.roll_1 :null}
+            {score.frame_4 && score.frame_4.roll_2 ? `, ${score.frame_4.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame5" colSpan='3'>
+            {score.frame_5 && score.frame_5.isStrike ? 'X' : null}
+            {score.frame_5 ? score.frame_5.roll_1 :null}
+            {score.frame_5 && score.frame_5.roll_2 ? `, ${score.frame_5.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame6" colSpan='3'>
+            {score.frame_6 && score.frame_6.isStrike ? 'X' : null}
+            {score.frame_6 ? score.frame_6.roll_1 :null}
+            {score.frame_6 && score.frame_6.roll_2 ? `, ${score.frame_6.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame7" colSpan='3'>
+            {score.frame_7 && score.frame_7.isStrike ? 'X' : null}
+            {score.frame_7 ? score.frame_7.roll_1 :null}
+            {score.frame_7 && score.frame_7.roll_2 ? `, ${score.frame_7.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame8" colSpan='3'>
+            {score.frame_8 && score.frame_8.isStrike ? 'X' : null}
+            {score.frame_8 ? score.frame_8.roll_1 :null}
+            {score.frame_8 && score.frame_8.roll_2 ? `, ${score.frame_8.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame9" colSpan='3'>
+            {score.frame_9 && score.frame_9.isStrike ? 'X' : null}
+            {score.frame_9 ? score.frame_9.roll_1 :null}
+            {score.frame_9 && score.frame_9.roll_2 ? `, ${score.frame_9.roll_2}` : null}
+          </td>
+          <td colSpan='3'></td><td id="frame10" colSpan='3'>
+            {score.frame_10 && score.frame_10.isStrike ? 'X' : null}
+            {score.frame_10 ? score.frame_10.roll_1:   null}
+            {score.frame_10 && score.frame_10.roll_2 ? `, ${score.frame_10.roll_2}` : null}
+          </td>
+        </tr>
+        <tr>
+          <td colSpan='6' id="marker0">{score.frame_1 && score.frame_1.roll_1 && score.frame_1.roll_2?   Number(score.frame_1.roll_1) + Number(score.frame_1.roll_2) : null}</td>
+          <td colSpan='6' id="marker1">{score.frame_2 ? score.frame_2.roll_1 : null}</td>
+          <td colSpan='6' id="marker2">{score.frame_3 ? score.frame_3.roll_1 : null}</td>
+          <td colSpan='6' id="marker3">{score.frame_4 ? score.frame_4.roll_1 : null}</td>
+          <td colSpan='6' id="marker4">{score.frame_5 ? score.frame_5.roll_1 : null}</td>
+          <td colSpan='6' id="marker5">{score.frame_6 ? score.frame_6.roll_1 : null}</td>
+          <td colSpan='6' id="marker6">{score.frame_7 ? score.frame_7.roll_1 : null}</td>
+          <td colSpan='6' id="marker7">{score.frame_8 ? score.frame_8.roll_1 : null}</td>
+          <td colSpan='6' id="marker8">{score.frame_9 ? score.frame_9.roll_1 : null}</td>
+          <td colSpan='6' id="marker9">{score.frame_10 ? score.frame_10.roll_1 : null}</td>
+        </tr>
       </tbody>
     </table>
   )
+
+
 }
 
 export default Table;
